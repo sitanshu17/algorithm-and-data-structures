bf3673cee35b528ce98467644b1d4ac73c0fe323
Sitanshu Shrestha <sitanshu.shrestha@gds.ey.com>
2022-01-11 17:07:19 +0530

    Linked List, String, Array

---
+++ Strings/ReverseWordsOfSentence.js
@@ -0,0 +1,52 @@
+/**
+ * Given a String S, reverse the string without reversing its individual words. Words are separated by dots.
+
+    Example 1:
+
+    Input:
+        S = i.like.this.program.very.much
+    Output: much.very.program.this.like.i
+    Explanation: After reversing the whole
+        string(not individual words), the input
+        string becomes
+        much.very.program.this.like.i
+    
+    Example 2:
+
+    Input:
+        S = pqr.mno
+    Output: mno.pqr
+    Explanation: After reversing the whole
+        string , the input string becomes
+        mno.pqr
+ */
+
+
+        const reverseWord = (sentence) => {
+            let wordArr = sentence.split('.');
+            let left = 0;
+            let right = wordArr.length - 1;
+
+            while (left < right) {
+                let temp = wordArr[left];
+                wordArr[left] = wordArr[right];
+                wordArr[right] = temp;
+                left++; 
+                right--;
+            }
+        
+            let rewResult = '';
+            let count = 0;
+            wordArr.forEach(element => {
+                count++;
+                if(wordArr.length == count) {
+                    rewResult = rewResult + element;
+                } else {
+                    rewResult = rewResult + element + '.';
+                }
+            });
+            return rewResult;
+        }
+        
+        let sentence = 'i.like.this.program.very.much';
+        console.log(reverseWord(sentence));
